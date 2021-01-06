be81462b1018bdd620873a5ad8fefe2821e0f1e5
chanaka prasad <igchanaka.prasad@gmail.com>
2021-01-06 18:48:27 +0530

    php file for navigation bar

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<head>
+    <title>NavBar</title>
+    <link rel="stylesheet" href="navbar.css">  <!--Copy and Paste this within Head Tag-->
+</head>
+
+<body>
+
+    <!--copy this whole thing (nav and script) on top of your body-->
+    <nav>
+        <div>
+            <a href="#"><img src="CeylonGig_logo.svg" alt="CeylonGig Logo"></a>
+        </div>
+        <ul class="nav-links">
+           
+            <li><a href="#" class="sign-up">Sign Up</a></li>
+          
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+
+    <script src="navbar.js"></script>
+
+</body>
+
+</html>
